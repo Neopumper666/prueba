12d0825c0fd0eba26d29860c249a87c2fc5adc3f
Juanpa <kgb_rnk@hotmail.com>
2013-06-25 01:07:45 -0500

    0.8

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-06-25 05:46:30 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-06-25	05:46:30	INFO	127.0.0.1	Joomla FAILURE: 	Usuario no existe
+2013-06-25	05:46:47	INFO	127.0.0.1	Joomla FAILURE: 	Usuario no existe
+2013-06-25	05:47:29	INFO	127.0.0.1	Joomla FAILURE: 	Contraseña inválida
